3c1bf3b9068ed4b69d9158dbb452db51d0ac7198
Megan <megan.koh96@gmail.com>
2023-12-14 21:22:24 +0800

    LOL my view0 html was in static and not template + annotated some thingies for test cause it wasnt working on its own

--- src/test/java/ibf2022/batch2/ssf/frontcontroller/FrontcontrollerApplicationTests.java
+++ src/test/java/ibf2022/batch2/ssf/frontcontroller/FrontcontrollerApplicationTests.java
@@ -3,7 +3,9 @@ package ibf2022.batch2.ssf.frontcontroller;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import vttp2023.batch3.ssf.frontcontroller.FrontcontrollerApplication;
+
+@SpringBootTest(classes = FrontcontrollerApplication.class)
 class FrontcontrollerApplicationTests {
 
 	@Test
